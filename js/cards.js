606c4ed3fec024bd539ee853e8d2a624597859e4
GustavopRezende <130861649+GustavopRezende@users.noreply.github.com>
2023-11-26 22:48:11 -0300

    botafogo 2.0

--- js/cards.js
+++ js/cards.js
@@ -1,107 +1,78 @@
-// Aguarde até que o DOM (Document Object Model) esteja totalmente carregado
-document.addEventListener('DOMContentLoaded', () => {
-    // Obtenha os parâmetros da URL, especificamente o número do jogador
-    const urlParams = new URLSearchParams(window.location.search);
-    const numeroJogador = urlParams.get('jogador');
+const url = "https://botafogo-atletas.mange.li";
+let playerNumbers = Array.from({ length: 60 }, (_, i) => i + 1);
 
-    // Função assíncrona para obter detalhes do jogador a partir do servidor
-    const fetchPlayerDetails = async (numero) => {
-        try {
-            // Converta o número do jogador para um inteiro
-            const numeroInteiro = parseInt(numero, 10);
+const pegar_coisas = async (endpoint) => {
+    const resposta = await fetch(`${url}/${endpoint}`);
+    const dados = await resposta.json();
+    return dados;
+}
 
-            // Verifique se o número do jogador é um inteiro válido
-            if (isNaN(numeroInteiro)) {
-                throw new Error(`Número do jogador não é um inteiro válido.`);
-            }
+const preenche = (atleta) => {
+    const container = document.createElement('div');
+    const titulo = document.createElement('h3');
+    const imagem = document.createElement('img');
 
-            // Faça uma requisição assíncrona para obter os detalhes do jogador do servidor
-            const resposta = await fetch(`https://botafogo-atletas.mange.li/${numeroInteiro}`);
+    container.className = atleta.elenco === 'feminino' ? 'card-feminino' : 'card-masculino';
+    titulo.innerText = atleta.nome;
+    imagem.src = atleta.imagem;
+    imagem.alt = `Imagem de ${atleta.nome}`;
 
-            // Verifique se a resposta da requisição é bem-sucedida (código de status 2xx)
-            if (!resposta.ok) {
-                throw new Error(`Erro na requisição: ${resposta.status} - ${resposta.statusText}`);
-            }
+    // Adiciona um evento de clique ao container do card
+    container.addEventListener('click', () => {
+        console.log("Número do jogador:", atleta.id);
+        redirecionarParaDetalhes(atleta.id);
+    });
 
-            // Parseie a resposta JSON obtida do servidor
-            const dados = await resposta.json();
+    container.appendChild(titulo);
+    container.appendChild(imagem);
 
-            // Verifique se os dados do jogador foram encontrados
-            if (!dados || Object.keys(dados).length === 0) {
-                throw new Error(`Dados do jogador ${numeroInteiro} não encontrados`);
-            }
+    document.getElementById('jogadores').appendChild(container);
+}
 
-            // Retorne os detalhes do jogador
-            return dados;
-        } catch (error) {
-            // Em caso de erro, registre no console, exiba uma mensagem de erro e propague o erro
-            console.error(`Erro ao obter detalhes do jogador ${numeroJogador}:`, error);
-            exibirErro(`Erro ao obter detalhes do jogador: ${error.message}`);
-            throw error;
-        }
-    };
+const limparJogadores = () => {
+    const containerJogadores = document.getElementById('jogadores');
+    containerJogadores.innerHTML = '';
+}
 
-    // Função para exibir detalhes do jogador na página
-    const displayPlayerDetails = (detalhes) => {
-        if (detalhes) {
-            // Mapeie os IDs dos elementos HTML aos dados do jogador e atualize o conteúdo
-            const elementIds = {
-                elenco: 'elenco',
-                nome: 'nome-jogador',
-                posicao: 'posicao-jogador',
-                imagem: 'imagem-jogador',
-                descricao: 'descricao-jogador',
-                nome_completo: 'nome-completo-jogador',
-                nascimento: 'nascimento-jogador',
-                altura: 'altura-jogador'
-            };
-
-            // Itere sobre os elementos e atualize seus conteúdos
-            Object.keys(elementIds).forEach(key => {
-                const element = document.getElementById(elementIds[key]);
-                if (element) {
-                    if (key === 'nascimento') {
-                        // Se a chave for 'nascimento', divida e formate a data de nascimento
-                        const [dataNascimento, localNascimento] = detalhes[key].split(', ');
-                        element.innerText = `Data de Nascimento: ${dataNascimento}\nLocal de Nascimento: ${localNascimento}`;
-                    } else {
-                        // Atualize o conteúdo do elemento com os dados do jogador
-                        element.innerText = detalhes[key];
-                        // Se a chave for 'imagem', atualize o atributo 'src' com a URL da imagem
-                        if (key === 'imagem') {
-                            element.src = detalhes[key];
-                        }
-                    }
-                }
-            });
-        } else {
-            // Em caso de dados não disponíveis, registre no console, exiba uma mensagem de erro
-            console.error(`Dados do jogador ${numeroJogador} não disponíveis.`);
-            exibirErro(`Dados do jogador não disponíveis.`);
-        }
-    };
-
-    // Função para exibir mensagens de erro na página
-    const exibirErro = (mensagem) => {
-        const elementoErro = document.getElementById('erro-jogador');
-        if (elementoErro) {
-            // Atualize o conteúdo do elemento de erro com a mensagem
-            elementoErro.innerText = mensagem;
-        }
-    };
-
-    // Verifique se o número do jogador está presente na URL
-    if (numeroJogador) {
-        // Se o número do jogador estiver presente, faça a chamada para obter detalhes e exibir na página
-        fetchPlayerDetails(numeroJogador)
-            .then(displayPlayerDetails)
-            .catch((error) => {
-                // Em caso de erro ao exibir detalhes, registre no console
-                console.error(`Erro ao exibir detalhes do jogador ${numeroJogador}:`, error);
-            });
-    } else {
-        // Se o número do jogador não estiver presente, registre no console e exiba uma mensagem de erro
-        console.error('Número do jogador não fornecido na URL.');
-        exibirErro('Número do jogador não fornecido na URL.');
+const buscarEExibirJogadores = async (endpoint) => {
+    try {
+        const data = await pegar_coisas(endpoint);
+        data.forEach((atleta) => {
+            preenche(atleta);
+        });
+    } catch (error) {
+        console.error("Erro ao buscar dados:", error);
     }
-});
+}
+
+const filtrarPorGenero = async (genero) => {
+    // Limpa os jogadores exibidos atualmente
+    limparJogadores();
+
+    let endpoint;
+    if (genero === 'Feminino') {
+        endpoint = 'feminino';
+    } else {
+        endpoint = 'masculino';
+    }
+
+    // Chama a função para buscar e exibir jogadores com base no novo filtro
+    buscarEExibirJogadores(endpoint);
+}
+
+const redirecionarParaDetalhes = (numeroJogador) => {
+    // Redireciona para a página de detalhes com o número do jogador na URL
+    window.location.href = `detalhes.html?jogador=${numeroJogador}`;
+    console.log(`Redirecionar para detalhes do jogador ${numeroJogador}`);
+}
+
+const verTodosJogadores = () => {
+    // Limpa os jogadores exibidos atualmente
+    limparJogadores();
+
+    // Chama a função para buscar e exibir todos os jogadores
+    buscarEExibirJogadores('all');
+}
+
+
+
